8c52bc3e8243f0881878b25a55ea2157ec3049fa
Edel Farah <edelfarah@Edels-MacBook-Pro.local>
2019-06-22 15:23:28 -0500

    final homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,48 @@ import os
 
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-x = []
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    for row in csvreader
- print(row)
+totalprofit = 0
+avgchange = []
+row_num = 0
+csvpath = os.path.join('budget_data.csv')
+date = []
+profit = []
 
+with open(csvpath, newline="") as csvfile:
+ csvreader = csv.reader(csvfile, delimiter=',')
+ header = next(csvreader)
+
+ for row in csvreader:
+   date.append(row[0])
+   profit.append(row[1])
+   totalprofit +=int(row[1])
+   currProfitFromThisRow = int(profit[len(profit)-1])
+   profitFromLastRow = int(profit[len(profit)-2])
+   if len(profit) == 1: #hack
+     continue
+   else:
+    avgchange.append(currProfitFromThisRow-profitFromLastRow)
+  
+import numpy as np
+avgChangeMinIndx = np.argmin(avgchange) + 1 #lmao wut
+avgChangeMaxIndx = np.argmax(avgchange) + 1 #lmao wut  
+
+
+print("Financial Analysis")
+print("------------------------")
+print("Total Months: " + str(len(date)))
+print("Total: " + "$"+str((totalprofit)))
+print("Average Change: $" + str(round(sum(avgchange)/len(avgchange),2)))
+print("Greatest Increase in Profits: " + str((date[avgChangeMaxIndx])) + "($" +  str(max(avgchange)) + ")")
+print("Greatest Decrease in Profits: " + str((date[avgChangeMinIndx])) + "($" +  str(min(avgchange)) + ")")
+
+text_file = open("PyBank.txt","w",newline='')
+
+text_file.write(("Finacial Analysis\n"))
+text_file.write("---------------------------\n")
+text_file.write("Total Months: " + str(len(date)) + "\n")
+text_file.write("Total: $" + str(totalprofit)+ "\n")
+text_file.write("Average Change: $" + str(round(sum(avgchange)/len(avgchange),2)) + "\n")
+text_file.write("Greates Increase in Profits: " + str((date[avgChangeMaxIndx])) +" ($" + str(max(avgchange)) + ")\n")
+text_file.write("Greates Decrease in Profits: " + str((date[avgChangeMinIndx])) +" ($" + str(min(avgchange)) + ")\n")
+text_file.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,53 @@
+import os
+import csv
+
+csvpath = os.path.join('election_data.csv')
+
+totalvotes = 0
+candidate = []
+candidates_dict = {}
+
+
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+
+        candy = row[2]
+        candidate.append(row[2])
+        totalvotes += 1
+
+        if candy not in candidates_dict:
+            candidates_dict[candy] = 1
+        else:
+            candidates_dict[candy] += 1
+
+winner = max(set(candidate), key=candidate.count)
+
+
+print("Election Results")
+print("---------------------------")
+print("Total Votes: " + str(totalvotes))
+print("---------------------------")
+
+
+for x in candidates_dict:
+     print(x + ": " + str(round((candidates_dict[x]/totalvotes)*100,2)) + "%" + "  (" + str(candidates_dict[x]) + ")")
+     
+
+print("---------------------------")    
+print("Winner: " + winner)
+print("---------------------------")   
+
+text_file = open("PyPoll.txt","w",newline='')
+
+text_file.write(("Election Results\n"))
+text_file.write("---------------------------\n")
+text_file.write(("Total Votes: " + str(totalvotes)) + "\n")
+text_file.write("---------------------------\n")
+text_file.write(((x + ": " + str(round((candidates_dict[x]/totalvotes)*100,2)) + "%" + "  (" + str(candidates_dict[x]) + ")")+ "\n"))
+text_file.write("---------------------------\n")
+text_file.write("Winner: " + winner + "\n")
+text_file.write("---------------------------\n")
+text_file.close()
